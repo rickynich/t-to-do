26562cf388945dca8c46789843e56aee61fa8fab
Nick Richard <nrich414@gmail.com>
2021-04-01 13:04:01 -0400

    install requirements, .env add (With temp port 5433)

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,8 +1,7 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
 
-class User(db.Model, UserMixin):
+class User(db.Model):
   __tablename__ = 'users'
 
   id = db.Column(db.Integer, primary_key = True)
