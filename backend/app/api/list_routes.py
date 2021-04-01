cbbb0e3b565cb8ffc2cb3b9ebd592835c92bcb70
Nick Richard <nrich414@gmail.com>
2021-04-01 14:42:25 -0400

    add table to model, update migrations

---
+++ backend/app/api/list_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify
+from app.models import List
+
+list_routes = Blueprint('lists', __name__)
+
+@list_routes.route('/')
+def lists():
+    lists = List.query.all()
+    return {"lists": [list.to_dict() for list in lists]}
+
+@user_routes.route('/<int:id>')
+def user(id):
+    list = List.query.get(id)
+    return list.to_dict()
--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -11,3 +11,49 @@ class User(db.Model):
       "id": self.id,
       "name": self.username,
     }
+
+class List(db.Model):
+    __tablename__ = 'lists'
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(60), nullable = False )
+
+    tasks = db.relationship("Task", back_populates="lists")
+
+    def to_dict(self):
+        return {
+        "id": self.id,
+        "title": self.title,
+        "tasks": [task.to_dict() for task in self.tasks]
+        }
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key = True)
+    list_id = db.Column(db.Integer, db.ForeignKey('lists.id'))
+    desc = db.Column(db.String(300), nullable = False )
+    status = db.Column(db.Boolean)
+
+    list = db.relationship('List', back_populates='tasks')
+
+    def to_dict(self):
+        return {
+        "id": self.id,
+        "desc": self.desc,
+        "status": self.status,
+        }
+
+class Comment(db.Model):
+    __tablename__="comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, db.ForeignKey('tasks.id'))
+    text = db.Column(db.String(300))
+
+    task = db.relationship('Task', back_populates='comments')
+    
+    def to_dict(self):
+        return {
+        "id": self.id,
+        "text": self.text,
