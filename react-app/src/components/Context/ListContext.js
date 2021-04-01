379061e077a879da3c00df165c76faf23be88587
Nick Richard <nrich414@gmail.com>
2021-04-01 17:34:18 -0400

    initial list context component complete

--- react-app/src/components/Context/ListContext.js
+++ react-app/src/components/Context/ListContext.js
@@ -1,2 +1,22 @@
 import React, { useContext, useState, useEffect } from "react";
 
+const ListContext = React.createContext();
+
+export function useList() {
+    return useContext(ListContext)
+}
+
+export function ListProvider({ children }) {
+    const [lists, setLists] = useState([])
+    
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch("/lists/")
+            const responseData = await response.json();
+            setLists(responseData.lists)
+        }
+        fetchData()
+    }, [lists])
+
+    return <ListContext.Provider value={lists}>{children}</ListContext.Provider>
+}
