4dd15ba8909198957de9f4226452249d98187386
Nick Richard <nrich414@gmail.com>
2021-04-04 13:31:36 -0400

    useReducer add and depency array parameter add for list update without page refresh

--- react-app/src/components/Context/ListContext.js
+++ react-app/src/components/Context/ListContext.js
@@ -1,20 +1,31 @@
-import React, { useContext, useState, useEffect } from "react";
+import { list } from "@chakra-ui/styled-system";
+import React, { useContext, useState, useEffect, useReducer } from "react";
 
 const ListContext = React.createContext({
     lists: [],
-    createNewList: () => {},
-	addTaskToList: () => {},
-	completeTask: (id) => {},
+    // createNewList: () => {},
+	// addTaskToList: () => {},
+	// completeTask: (id) => {},
 });
 
 export function useList() {
 	return useContext(ListContext);
 }
 
+function reducer(state, action) {
+	switch (action.type) {
+		case "add-list":
+			console.log("State in reducer function: ", state, "incoming value: ",action.value)
+			return {...state, lists: action.value}
+	}
+}
+
 export function ListProvider({ children }) {
 	const [lists, setLists] = useState([]);
 	const [selectedList, setSelectedList] = useState();
+	const [state, dispatch] = useReducer(reducer, lists) //add initial state to have current lists 
 
+	//initial load
 	useEffect(() => {
 		async function fetchData() {
 			const response = await fetch('/lists/');
@@ -22,11 +33,10 @@ export function ListProvider({ children }) {
 			setLists(responseData.lists);
 		}
 		fetchData();
-	}, []);
+	}, [state]);
 
     async function createNewList(title) {
         console.log("CREATE NEW LIST HIT. title:", title)
-
         const response = await fetch('/lists/', {
             method: 'POST',
             headers: {
@@ -36,17 +46,15 @@ export function ListProvider({ children }) {
                 title
             })
             });
-			// const responseData = await response.json();
-			return await response.json();
-
+			const newListResponseData = await response.json();
+			dispatch({ type: "add-list", value: newListResponseData }); //change to redux naming conventions of constants at top
+			return 
 	}
-	// function addTaskToList(listId) {
 
-	// }
 	function completeTask(taskId) {
 		setSelectedList();
 	}
-	// console.log("Lists", lists)
+
 	return (
 		<ListContext.Provider value={{ lists, completeTask, createNewList }}>
 			{children}
